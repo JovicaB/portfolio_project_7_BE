cc3fcff196d785bdcdbb60590b71b463fc2cdda6
Jovica Božić <112812542+JovicaB@users.noreply.github.com>
2023-11-27 15:39:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-###
+# 1. save stock values into JSON: current_day_prices.json
+# 2. contruct data for DB (difference in values=last tday--current day)
+# 3. save JSON str in DB 
+# 4. overwrite last_day_prices.json with current_day_prices.json
+# 5. clear current_day_prices.json
